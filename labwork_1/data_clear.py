75d6487a81bd13d63b3008cd7501890c809dc4bb
Nguyen Manh Hung <hungnm.22bi13183@usth.edu.vn>
2024-05-09 11:37:35 +0700

    clean weather data

--- labwork_1/data_clear.py
+++ labwork_1/data_clear.py
@@ -10,11 +10,19 @@ with open(join(getPath(),'weather_data.csv'),'r') as csv_file:
     lines = csv_file.readlines()
     for line in lines:
         try:
-            date = line.split(",")[0]
-            time = date.split()[1]
-            hour = time.split(":")[0]
-            if int(hour)%8 == 0:
-                print(date)
+            oo = line.split(",")
+            print(oo)
+            missData = 0
+            for col in range(3,9):
+                if col == 8:
+                    oo[col] = oo[col][:-1]
+                if oo[col] == "0.0" or oo[col] == "0":
+                    missData = 1
+                    break
+            if missData == 1:
+                print("deleted")
+                continue
+            else:
                 with open(join(getPath(),"new_data.csv"),'a') as new_file:
                     new_file.write(line)
         except ValueError:
